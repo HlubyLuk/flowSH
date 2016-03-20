8ac256e0b636a457c4eaeca2d53365eba1f2399c
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2016-03-20 15:45:57 +0100

    Fix of finish feateru
    Don't need name of current feature, which you finish.

--- flowScripts/finishFeature.sh
+++ flowScripts/finishFeature.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+currentBranch=`git rev-parse --abbrev-ref HEAD` ;
 git add -i ;
 git commit -a ;
 git checkout develop ;
-git merge --no-ff $1 ;
+git merge --no-ff ${1:-$currentBranch} ;
