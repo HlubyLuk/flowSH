e5339b0bddfe9e8051e25d00cfb152ad2bb31999
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2016-04-02 13:17:12 +0200

    Change log fix

--- flowScripts/changeLog.sh
+++ flowScripts/changeLog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 touch CHANGELOG.txt
-echo "***`git rev-parse --verify HEAD`" >> CHANGELOG.txt
-git log --no-color --oneline --no-merges HEAD~1..HEAD >> CHANGELOG.txt
+echo "*** `git log --max-count=1 --pretty=%h`" >> CHANGELOG.txt
+git log --no-color --no-merges --pretty=%h\ %an%n\ \ \ \ %s%n\ \ \ \ %b HEAD~1..HEAD >> CHANGELOG.txt
